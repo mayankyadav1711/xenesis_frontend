e42f135210fe20b6aad360eb0dbeb4fc5c7fa973
Vishwang0Suthar <dripydacoder@gmail.com>
2024-01-25 12:08:16 +0530

    custom color added tailwind

--- src/components/myticket.js
+++ src/components/myticket.js
@@ -4,7 +4,7 @@ import Ticket from "./ticket/ticket";
 
 function Myticket(Props) {
     return (
-        <div className=" px-10 py-10 ">
+        <div className=" px-10 py-10 bg-darkBlue-50">
             <h4 className="text-center text-4xl font-bold pb-10">Ticket</h4>
 
             <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
