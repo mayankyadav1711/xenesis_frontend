a9d2283d0f067119dc80a3d7ab6cd1225777053e
Harshil Valiya <harshilvaliya40@gmail.com>
2024-01-25 15:37:59 +0530

    minor improvments

--- src/components/eventDetailsPage/eventDetailsSections/eventOrganizer.js
+++ src/components/eventDetailsPage/eventDetailsSections/eventOrganizer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SectionTitle from "../eventDetailsComponents/sectionTitle";
 
 const EventOrganizer = () => {
   // Organizers data array
@@ -26,22 +27,26 @@ const EventOrganizer = () => {
   ];
 
   return (
-    <div className="md:flex grid grid-cols-2 items-center lg:justify-start justify-center lg:gap-10 gap-5 sm:gap-y-8 mt-10 organizers-div">
-      {organizersData.map((organizer) => (
-        <div
-          key={organizer.id}
-          className="flex flex-col items-center justify-center"
-        >
-          <img
-            src={organizer.imageSrc}
-            alt=""
-            className="xl:w-[200px] lg:w-[160px] md:w-[140px] w-[160px] xl:h-[200px] lg:h-[160px] md:h-[140px] h-[160px] object-cover object-right rounded-full org-images"
-          />
-          <p className="mt-3 text-center lg:text-2xl sm:text-xl text-lg lg:font-bold font-semibold org-name">
-            {organizer.name}
-          </p>
-        </div>
-      ))}
+    <div>
+      <SectionTitle title="Organizers" />
+
+      <div className="md:flex grid grid-cols-2 items-center lg:justify-start justify-center lg:gap-10 gap-5 sm:gap-y-8 mt-10 organizers-div">
+        {organizersData.map((organizer) => (
+          <div
+            key={organizer.id}
+            className="flex flex-col items-center justify-center"
+          >
+            <img
+              src={organizer.imageSrc}
+              alt=""
+              className="xl:w-[200px] lg:w-[160px] md:w-[140px] w-[160px] xl:h-[200px] lg:h-[160px] md:h-[140px] h-[160px] object-cover object-right rounded-full org-images"
+            />
+            <p className="mt-3 text-center lg:text-2xl sm:text-xl text-lg lg:font-bold font-semibold org-name">
+              {organizer.name}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
