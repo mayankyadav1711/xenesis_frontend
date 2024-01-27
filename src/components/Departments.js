6bc6f07de45f3936f7739f28318caa112f4513bd
ArjunCJ <arjunlatiwala@gmail.com>
2024-01-27 09:39:30 +0530

    home page 1

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -1,32 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Navigation, Pagination, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import React, { useEffect, useState } from "react";
+import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Departments = () => {
   const departments = [
-    ["Computer Engineering", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/com_poggy8.png"],
-    ["Information Technology", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252583/IT_fxhzmv.png"],
-    ["Mechanical Engineering", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252585/Mech_yysew0.png"],
-    ["Electrical Engineering", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/Elec_t062k6.png"],
-    ["Electronics Communication", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/EC_hgsynt.png"],
-    ["MBA Department", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252583/MBA_y1dusl.png"],
-    ["MCA Department", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252584/MCA_skulpx.png"],
-    ["Science and Humanity", "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252586/SH_jeidwy.png"],
+    [
+      "Computer Engineering",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/com_poggy8.png",
+    ],
+    [
+      "Information Technology",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252583/IT_fxhzmv.png",
+    ],
+    [
+      "Mechanical Engineering",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252585/Mech_yysew0.png",
+    ],
+    [
+      "Electrical Engineering",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/Elec_t062k6.png",
+    ],
+    [
+      "Electronics Communication",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252582/EC_hgsynt.png",
+    ],
+    [
+      "MBA Department",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252583/MBA_y1dusl.png",
+    ],
+    [
+      "MCA Department",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252584/MCA_skulpx.png",
+    ],
+    [
+      "Science and Humanity",
+      "https://res.cloudinary.com/db4e3hqfv/image/upload/v1706252586/SH_jeidwy.png",
+    ],
   ];
 
   return (
-    <div className="overflow-x-hidden bg-cover bg-no-repeat bg-center bg-[#070d1590] backdrop-filter backdrop-blur-3xl" style={{
-        backgroundImage: "url('https://res.cloudinary.com/db4e3hqfv/image/upload/v1706210892/view-3d-space-rocket-model_obtkw7.jpg')",
-      }}>
-       <div className="absolute inset-0 bg-[#070d15c7] backdrop-filter backdrop-blur-3xl"></div>
-       
+    <div
+      className="overflow-x-hidden bg-cover bg-no-repeat bg-center backdrop-filter "
+      style={{
+        backgroundImage: "../../public/icons/Frame 3.svg",
+      }}
+    >
+      <div className="absolute inset-0 backdrop-filter backdrop-blur-3xl"></div>
+
       {/* Departments */}
-      <section className="radial-gradient-container w-full py-[calc(3vh)] relative overflow-hidden">
-      
+      <section className="radial-gradient-container w-full py-8 relative overflow-hidden">
+        <div className="w-full text-center">
+          <h3 className="uppercase text-white text-heading5 font-serif font-semibold py-7">
+            Departments
+          </h3>
+        </div>
         <Swiper
           className="ml-8 swiper-container-glass"
           modules={[Navigation, Pagination, A11y]}
@@ -34,8 +65,8 @@ const Departments = () => {
           slidesPerView={3}
           pagination={{ clickable: true }}
           navigation={{
-            nextEl: '.custom-next-button',
-            prevEl: '.custom-prev-button',
+            nextEl: ".custom-next-button",
+            prevEl: ".custom-prev-button",
           }}
           breakpoints={{
             320: {
@@ -59,13 +90,15 @@ const Departments = () => {
             {departments.map((department, index) => (
               <SwiperSlide key={index}>
                 <div
-                  onClick={() => (window.location.href = `/events#${department[0]}`)}
+                  onClick={() =>
+                    (window.location.href = `/events#${department[0]}`)
+                  }
                   data-aos="zoom-in"
                   className="w-full rounded-lg bg-white/10 backdrop-blur-md md:md:mx-2 transition-all duration-300 transform hover:scale-105 shadow-md overflow-hidden card-3d mt-4 mb-6"
                   style={{
-                    background: 'rgba(255, 255, 255, 0.1)',
-  boxShadow: '6px 6px 6px 6px rgba(0, 0, 0, 0.25)', // Outset box shadow
-  transform: 'hover:scale-105',
+                    background: "rgba(255, 255, 255, 0.1)",
+                    boxShadow: "6px 6px 6px 6px rgba(0, 0, 0, 0.25)", // Outset box shadow
+                    transform: "hover:scale-105",
                   }}
                 >
                   <div className="overflow-hidden rounded-t-lg">
@@ -75,16 +108,22 @@ const Departments = () => {
                       className="h-52 w-full object-cover transform hover:scale-110 transition-all duration-300"
                     />
                   </div>
-                  <h1 className="p-4 text-white font-bold text-center">{department[0]}</h1>
+                  <h1 className="p-4 text-white font-bold text-center">
+                    {department[0]}
+                  </h1>
                 </div>
               </SwiperSlide>
             ))}
           </div>
           <div className="custom-prev-button absolute top-1/2 left-0 transform -translate-y-1/2 bg-black dark:bg-gray-800 rounded-full p-2 ml-4 cursor-pointer">
-            <i className="text-white">{/* Your custom previous arrow icon goes here */}</i>
+            <i className="text-white">
+              {/* Your custom previous arrow icon goes here */}
+            </i>
           </div>
           <div className="custom-next-button absolute top-1/2 right-0 transform -translate-y-1/2 bg-black dark:bg-gray-800 rounded-full p-2 mr-4 cursor-pointer">
-            <i className="text-white">{/* Your custom next arrow icon goes here */}</i>
+            <i className="text-white">
+              {/* Your custom next arrow icon goes here */}
+            </i>
           </div>
         </Swiper>
       </section>
