8da3d679b5f188aadf8437e9de747838f13c338f
Vishwang0Suthar <dripydacoder@gmail.com>
2024-01-27 10:15:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Register from "./components/register";
 import OTP from "./components/OTP";
 import ResetPassword from "./components/ResetPassword";
 import ForgotPassword from "./components/ForgotPassword";
-import Events from "./components/Events";
+import Events from "./components/events";
 import "./globals.css"
 
 import Verifier from "./components/Verifier/verifier";
